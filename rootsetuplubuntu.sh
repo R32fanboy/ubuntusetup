83b55efe9309bb8a19b18d8724283b0b2db474e7
R32fanboy <32889334+R32fanboy@users.noreply.github.com>
2019-03-23 04:22:10 -0500

    Add files via upload

---
+++ rootsetuplubuntu.sh
@@ -0,0 +1,9 @@
+apt update && apt upgrade -y
+apt install lubuntu-core xorg -y
+apt install firefox kdenlive telegram-desktop audacity handbrake virt-manager libvirt-bin krita vlc htop screenfetch powertop p7zip-full qml-module-qtquick-dialogs qml-module-qtquick-privatewidgets transmission -y
+wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+dpkg -i teamviewer_amd64.deb
+remove chromium-browser -y
+cd ..
+rm -r ubuntusetup
+reboot now
