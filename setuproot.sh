37f335619d3fb1abb62d575271f20f64ffd1030e
R32fanboy <32889334+R32fanboy@users.noreply.github.com>
2019-03-23 02:59:21 -0500

    Update setuproot.sh

--- setuproot.sh
+++ setuproot.sh
@@ -1,4 +1,4 @@
-apt update && sudo apt upgrade -y
+apt update && apt upgrade -y
 apt install firefox kdenlive telegram-desktop audacity handbrake virt-manager libvirt-bin krita vlc htop screenfetch powertop p7zip-full qml-module-qtquick-dialogs qml-module-qtquick-privatewidgets transmission -y
 wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 dpkg -i teamviewer_amd64.deb
