d3d39c4da1997c24ae00119584e31c2dd34e58d4
R32fanboy <32889334+R32fanboy@users.noreply.github.com>
2019-03-23 02:37:37 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install firefox kdenlive telegram-desktop audacity handbrake virt-manager libvirt-bin krita vlc htop screenfetch powertop p7zip-full qml-module-qtquick-dialogs qml-module-qtquick-privatewidgets transmission -y
 wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 sudo dpkg -i teamviewer_amd64.deb
-sudo remove chromium-browser -y
+sudo apt remove chromium-browser -y
 sudo reboot now
